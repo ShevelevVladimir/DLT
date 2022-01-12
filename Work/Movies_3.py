6121e617f744a470b4feab9cf9f745d294d670c9
ShevelevVladimir <92126993+ShevelevVladimir@users.noreply.github.com>
2022-01-12 17:58:58 +1000

    Create Movies_3.py

---
+++ Work/Movies_3.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+users = pd.read_csv('users.csv', header=None, engine = 'python',names=["UserID","Gender","Age","Occupation","Zip-code"], delimiter='::')
+rating = pd.read_csv('rating.csv', header=None, engine = 'python',names=["UserID","MovieID","Rating","Timestamp"], delimiter='::')
+new = pd.merge(rating,users)
+df = pd.pivot_table(new, index=["Gender","Age"])
+del df["MovieID"]
+del df["Occupation"]
+del df["Timestamp"]
+del df["UserID"]
+g2=df.sort_values(by="Rating",ascending=False)
+g2
